9b726ff4629d2fb547b9e6b71e30446ae133c9e1
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-06-08 17:08:19 -0400

    comienzan los ejercicios de Ror en inforcap + clase 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
   get "/news", to: "news#index"
